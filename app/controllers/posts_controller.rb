886ea05d51af9a959ea11f278fe2f1da19909b15
AleksandarJeftic <aleksandar@axiomq.com>
2022-10-17 11:11:50 +0200

    Fixed turbo stream remove target

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.update(post_params)
         format.turbo_stream do
-          render turbo_stream: turbo_stream.replace(@post, partial: "posts/post_row", locals: { post: @post })
+          render turbo_stream: turbo_stream.replace("list_post_#{@post.id}", partial: "posts/post_row", locals: { post: @post })
         end
         format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
         format.json { render :show, status: :ok, location: @post }
@@ -58,7 +58,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.remove(@post) }
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("list_post_#{@post.id}") }
       format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
     end
