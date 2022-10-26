82bea4a53bbe53a6e552dad0e34d1937088727e8
AleksandarJeftic <aleksandar@axiomq.com>
2022-10-26 23:33:01 +0200

    Added pagy pagination with bootstrap styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.order(created_at: :asc)
+    @pagy, @posts = pagy(Post.order(created_at: :asc), items: 10)
   end
 
   # GET /posts/1 or /posts/1.json
